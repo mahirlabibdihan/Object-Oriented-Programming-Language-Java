a4fa24555de29e7dce126a1b416afd21236940b6
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-02-07 17:47:20 +0600

    main

---
+++ Class.java
@@ -0,0 +1,14 @@
+public class Animal{
+	private String name="Dihan";
+	public void PrintName()
+	{
+		System.out.println(this.name);
+	}
+}
+
+public class Class{
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ OOP.java
@@ -0,0 +1,23 @@
+	class Animal{
+	public String name="Dihan";
+	public void PrintName()
+	{
+		System.out.println(this.name);
+	}
+
+	}
+
+	class Dog extends Animal{
+		@Override
+		public void PrintName()
+		{
+			System.out.println("Nahid");
+		}
+		
+	}
+public class OOP{
+	public static void main(String[] args)
+	{
+		
+	}
+}
